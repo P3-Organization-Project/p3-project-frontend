b5d91ac1bda0fa4b04d02d91fea009cb03ca842f
StagnantBytes <artyom.shevchenko2005@gmail.com>
2025-10-17 11:13:17 +0200

    refactoring the other front end project evything should be the same now

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import backgroundImage from './assets/LoginPageBackground.jpg';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    return (
+        <>
+
+            <div className="h-screen w-screen overflow-hidden">
+                <div style={{ backgroundImage: `url(${backgroundImage})` }} className="bg-cover bg-center h-screen w-full flex justify-center items-center w-screen h-screen " >
+                    <div className="w-150 h-110 p-5 shadow-lg bg-black rounded-md">
+                        <h1 className="text-3x1 block text-center font-semibold"><i className="fa-solid fa-user"></i>Login</h1>
+                        <hr className="mt-5"/>
+                        <div className="mt-5">
+                            <label For="username" className="block text-base mb-2">Username</label>
+                            <input type="text" id="username"
+                                   className="border w-full text-base px-3 py-3 focus:outline-none focus:ring-0 focus:border-gray-600"
+                                   placeholder="Enter Username..."/>
+                        </div>
+                        <div className="mt-3">
+                            <label For="password" className="block text-base mb-2">Password</label>
+                            <input type="password" id="password"
+                                   className="border w-full text-base px-3 py-3 focus:outline-none focus:ring-0 focus:border-gray-600"
+                                   placeholder="Enter Password..."/>
+                        </div>
+                        <div className="mt-3 flex justify-between items-center">
+                            <div>
+                                <a href="#" className="text-indigo-800 font-semibold">Forgot Password?</a>
+                            </div>
+                        </div>
+                        <div className="mt-10">
+                            <button type="submit" className="border-2 border-indigo-700 bg-indigo-700 text-white py-1 px-5 w-full rounded-md hover:bg-transparent hover:text indigo-700 font-semibold">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    theme:{
+        extend:{backgroundImage:{'LoginPageBackground':"url('/src/assets/LoginPageBackground.png')",},},
+    },
+    plugins: [react(), tailwindcss()],
 })
