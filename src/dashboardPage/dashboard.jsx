8b8ca6920ee4d04f4ccb13ee8a4572cf7aad3bea
Askernl <asker.nl@hotmail.com>
2025-10-21 18:38:07 +0200

    made the dashboard much cleaner

--- src/dashboardPage/dashboard.jsx
+++ src/dashboardPage/dashboard.jsx
@@ -3,43 +3,18 @@ import {useNavigate} from "react-router-dom";
 
 function Dashboard() {
 
-    const caseNavigate = useNavigate()
-    const GoToCase = () => {
-        caseNavigate("/case");
-    }
+    const navigate = useNavigate();
 
-    const clientNavigate = useNavigate()
-    const GoToClient = () => {
-        clientNavigate("/Client");
-    }
-
-    const materialerNavigate = useNavigate()
-    const GoToMaterialer = () => {
-        materialerNavigate("/materialer");
-    }
-
-    const opretcaseNavigate = useNavigate()
-    const GoToOpretcase = () => {
-        opretcaseNavigate("/opretcase");
-    }
-
-    const doorNavigate = useNavigate()
-    const GoToDoor = () => {
-        doorNavigate("/door");
-    }
-    const teamNavigate = useNavigate()
-    const GoToTeam = () => {
-        teamNavigate("/team");
-    }
+    const goTo = (path) => () => navigate(path);
         return (
             <div className="flex items-center justify-center h-screen">
                 <h1 className="text-3xl font-bold">Welcome to the Dashboard 🎉</h1>
-                <button onClick={GoToCase}>Case oversigt</button>
-                <button onClick={GoToClient}>Klient oversigt</button>
-                <button onClick={GoToMaterialer}>materiale oversigt</button>
-                <button onClick={GoToOpretcase}>opret case</button>
-                <button onClick={GoToDoor}>dør</button>
-                <button onClick={GoToTeam}>hold</button>
+                <button onClick={goTo("/case")}>Case</button>
+                <button onClick={goTo("/client")}>Klient</button>
+                <button onClick={goTo("/materialer")}>Materialer</button>
+                <button onClick={goTo("/opretcase")}>Opret Case</button>
+                <button onClick={goTo("/door")}>Dør</button>
+                <button onClick={goTo("/team")}>Team</button>
 
             </div>
 
