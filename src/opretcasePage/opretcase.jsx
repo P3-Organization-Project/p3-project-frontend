67b589c5d0cbda82e4a168196c5d7e254403c57e
HainsoTD <dando3007@gmail.com>
2025-10-27 00:23:09 +0100

    Modified layout on "opretcase"

--- src/opretcasePage/opretcase.jsx
+++ src/opretcasePage/opretcase.jsx
@@ -2,8 +2,6 @@ import './opretcase.css'
 import { useNavigate } from 'react-router-dom'
 import React from "react";
 
-
-
 function Opretcase() {
     const navigate = useNavigate()
     const goToCaseOverview = () => {
@@ -14,23 +12,46 @@ function Opretcase() {
         <div className="flex flex-col items-center justify-center h-screen">
             <h1>Create Case 🎉</h1>
 
-            <h2>Choose Client*</h2>
-            <h2>Add Door Model</h2>
-            <h2>Added Doors</h2>
-            <h2>Status</h2>
+            <div className="case-main">
+                {/*Client*/}
+                <div className="client-section">
+                    <label className="client-label">Choose Client *</label>
+                    <select className="client-dropdown">
+                        <option>-----</option>
+                    </select>
 
-            <div className="mt-4 flex flex-col gap-2">
-                <button className="opretcase-chooseClient">Choose Client</button>
-                <button className="opretcase-createClient">+ Create New Client</button>
+                    <button className="opretcase-createClient">+ Create New Client</button>
+                </div>
+
+                {/*Door*/}
+                <h2>Add Door Model</h2>
                 <button className="opretcase-catalogue-btn">Door Catalogue</button>
-                <button className="opretcase-status-btn">Change Status</button>
-                <button className="opretcase-cancel-btn"
-                        onClick={goToCaseOverview}
-                        type="submit">Cancel</button>
-                <button className="opretcase-create-btn">Create Case</button>
+
+                <h2>Added Doors</h2>
+
+                {/*Status*/}
+            <div className="opretCase-status">
+                <div className="status-box">
+                    <label className="status-label">Status *</label>
+                    <select className="status-dropdown">
+                        <option>-----</option>
+                    </select>
+                </div>
+
+        
             </div>
-
-
+            </div>
+            {/*Action Buttons*/}
+                <div className="opretcase-action-btn">
+                    <button
+                        className="opretcase-cancel-btn"
+                        onClick={goToCaseOverview}
+                        type="submit"
+                    >
+                        Cancel
+                    </button>
+                    <button className="opretcase-create-btn">Create Case</button>
+                </div>
         </div>
     )
 }
