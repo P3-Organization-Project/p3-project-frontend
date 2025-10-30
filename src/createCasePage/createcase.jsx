559dabdcda18233a60f6b94dc5d6d1c295e5fc7c
HainsoTD <dando3007@gmail.com>
2025-10-30 11:58:29 +0100

    Change layout
    "Create New client " is align

--- src/createCasePage/createcase.jsx
+++ src/createCasePage/createcase.jsx
@@ -9,28 +9,32 @@ function Createcase() {
     }
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-white">
+        <div className="flex flex-col items-center justify-center h-screen w-screen bg-white">
             <h1>Create Case 🎉</h1>
 
             <div className="case-main">
                 {/*Client*/}
                 <div className="client-section">
                     <label className="client-label">Choose Client *</label>
-                    <select className="client-dropdown">
-                        <option>-----</option>
-                    </select>
 
-                    <button className="opretcase-createClient">+ Create New Client</button>
+                    <div className="client-controls">
+                        <select className="client-dropdown">
+                            <option>-----</option>
+                        </select>
+
+                        <button className="createcase-createClient">+ Create New Client</button>
+                    </div>
                 </div>
+
 
                 {/*Door*/}
                 <h2>Add Door Model</h2>
-                <button className="opretcase-catalogue-btn">Door Catalogue</button>
+                <button className="createcase-catalogue-btn">Door Catalogue</button>
 
                 <h2>Added Doors</h2>
 
                 {/*Status*/}
-            <div className="opretCase-status">
+            <div className="createcase-status">
                 <div className="status-box">
                     <label className="status-label">Status *</label>
                     <select className="status-dropdown">
@@ -42,15 +46,15 @@ function Createcase() {
             </div>
             </div>
             {/*Action Buttons*/}
-                <div className="opretcase-action-btn">
+                <div className="createcase-action-btn">
                     <button
-                        className="opretcase-cancel-btn"
+                        className="createcase-cancel-btn"
                         onClick={goToCaseOverview}
                         type="submit"
                     >
                         Cancel
                     </button>
-                    <button className="opretcase-create-btn">Create Case</button>
+                    <button className="createcase-create-btn">Create Case</button>
                 </div>
         </div>
     )
