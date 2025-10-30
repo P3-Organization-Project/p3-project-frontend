c7fd010b2016455ad5799754a05df6e0964b6ee8
GabsBlake <Gabrielluurblake@outlook.com>
2025-10-30 12:03:45 +0100

    door catalog buttons added

--- src/createCasePage/catalogue.jsx
+++ src/createCasePage/catalogue.jsx
@@ -7,10 +7,17 @@ function Catalogue() {
 
     const goTo = (path) => () => navigate(path);
     return (
-        <div className="flex items-center justify-center h-screen">
-            <h1 className="text-3xl font-bold">Welcome to the catalogue oversigt 🎉</h1>
-            <button onClick={goTo("/")}className>DoorA</button>
+       <div className="flex flex-col items-center justify-center justify-start h-screen w-screen bg-white text-black">
+        Door Catalog
+
+
+        <div className="flex gap-10 mt-4" >
+            <button onClick={goTo("/catalogue")} className="px-4 py-2 bg-gray-200 rounded w-20 h-10 text-white"> 1</button>
+            <button onClick={goTo("/practical")} className="px-4 py-2 bg-gray-200 rounded w-20 h-10 text-white"> 2</button>
+            <button onClick={goTo("/catalogue")} className="px-4 py-2 bg-gray-200 rounded w-20 h-10 text-white"> 3</button>
+            <button onClick={goTo("/catalogue")} className="px-4 py-2 bg-gray-200 rounded w-20 h-10 text-white"> 4</button>
         </div>
+    </div>
     )
 }
 export default Catalogue
